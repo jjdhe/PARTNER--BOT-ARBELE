81903bbd14902484f908fc4bd7406458a8df8aca
jjdhe <76827570+jjdhe@users.noreply.github.com>
2021-01-01 03:48:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(``) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(`https://discord.gg/VkPX4tkfr2`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("id جەنالەکە").send(
+  client.channels.get("781974291851771904").send(
 `> sent By  <@${message.author.id}> ✅ 
 ${message.content}`)
 }
@@ -51,10 +51,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`شتێک بنوسە بۆ ستریمینگ`,"http://twitch.tv/PAYWANDcamper") 
+client.user.setGame(ڕیکلامەکە بنێرە بۆمن`٥شتێک بنوسە بۆ ستریمینگ`,"http://twitch.tv/PAYWANDcamper") 
 });
 
 
 
 
-client.login("");//تۆکین لێرە دانێ
+client.login("Nzk0MzYzMDg5ODUxMTIxNzA1.X-5uRw.AROdpz2Gycg2n8BOyMpfYnyXvU4");//تۆکین لێرە دانێ
